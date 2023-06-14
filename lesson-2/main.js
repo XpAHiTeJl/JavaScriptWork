d6276c8c6603414b0bec8b42a7816384a739373c
XpAHiTeJl <robengud2@gmail.com>
2023-06-15 00:39:40 +0300

    Work

---
+++ lesson-2/main.js
@@ -0,0 +1,17 @@
+"use strict";
+var number = 20;
+var firstRegistred = prompt("Хочешь поиграть в  игру?  ");
+
+if (firstRegistred === "да") {
+  console.log("Хорошо, стартуем!");
+  var firstNum = parseInt(prompt("Сколько мощи ты выбирешь ?   "));
+  if (firstNum === number) {
+    console.log("Ты в игре, поздравляю");
+  } else if (firstNum !== number) {
+    console.log("Попробуй еще раз");
+  }
+} else if (firstRegistred === "нет") {
+  console.log("Всего хорошего");
+} else {
+  console.log("Ты должен сделать конкретный выбор!");
+}
