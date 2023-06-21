bb92bacaa8700cd45d0d69a9042292ff4de11994
XpAHiTeJl <robengud2@gmail.com>
2023-06-21 18:34:51 +0300

    email

--- lesson-2/homeWork/main.js
+++ lesson-2/homeWork/main.js
@@ -1,24 +1,29 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+"use strict";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+var invalidData = {
+  userName: true,
+  userEmail: true,
+  userPassword: true,
+  userAge: true,
+};
 
-setupCounter(document.querySelector('#counter'))
+var userEmail = prompt("Каков твой Эмэил ?");
+var userPassword = prompt("Какой у тебя пароль?");
+var userAge = prompt("Сколько тебе лет ?");
+
+while (invalidData.userName) {
+  var userName = prompt("Как тебя зовут ?");
+
+  if (userName.length > 2) invalidData.userName = false;
+}
+
+if (invalidData.userName === false) {
+  console.log("Имя корректное!");
+
+  while (invalidData.userEmail) {
+    for (var i = 0; i < userEmail.length; i++) {
+      var char = userEmail[i];
+      if (char === "@") invalidData.userEmail = false;
+    }
+  }
+}
