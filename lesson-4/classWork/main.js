0cb7a8757a39ca4a2ace116aac04710510117277
XpAHiTeJl <robengud2@gmail.com>
2023-06-29 20:20:06 +0300

    lesson4

---
+++ lesson-4/classWork/main.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+// Task
+// 1) Validation for : name and age
+// name - should contain : . as a first character in the string
+// name - should contain : !  as a last character in the string
+
+// if name is correct
+
+// 2) age - user age should divide on 4 without part after divide operation
+
+var validation = {
+  isValidName: false,
+  isValidAge: false,
+};
+
+while (!validation.isValidName) {
+  var userName = prompt("Как тебя зовут?");
+  // Kir
+  // 0123
+  if (userName[0] === "." && userName[userName.length - 1] === "!") {
+    validation.isValidName = true;
+    break;
+  }
+}
+var userAge = promt("Возраст");
+if (userAge % 4 === 0) {
+}
+console.log("Возраст четкий");
