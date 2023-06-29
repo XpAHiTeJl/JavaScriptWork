05cf231a6cf38b5e2a96f7af528a930dde382b72
XpAHiTeJl <robengud2@gmail.com>
2023-06-29 20:57:33 +0300

    lesson-4.1

--- lesson-4/homeWork/main.js
+++ lesson-4/homeWork/main.js
@@ -1,3 +1,18 @@
-
 "use strict";
 
+const lvl = "#";
+
+const lastLvlDungeons = "######" .length;
+
+let lvlDungeon = "";
+
+const newLvlDungeon = "\n";
+
+console.log('[lstLvl]', lastLvlDungeons );
+
+// * добавление каждому этажу # * i + абзац ( \n )
+for(let i = 1 ; i < lastLvlDungeons + 1 ; i++){
+  
+  lvlDungeon += lvl.repeat(i) + newLvlDungeon;
+}
+console.log(lvlDungeon)
