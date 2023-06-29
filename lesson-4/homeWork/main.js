7eb71bcfd1f760c7534cb4bedc11877c88c3eb27
XpAHiTeJl <robengud2@gmail.com>
2023-06-29 22:36:23 +0300

    lesson-4

--- lesson-4/homeWork/main.js
+++ lesson-4/homeWork/main.js
@@ -1,18 +1,69 @@
 "use strict";
 
-const lvl = "#";
 
-const lastLvlDungeons = "######" .length;
 
-let lvlDungeon = "";
+//  дз номер 1 сделать пирамиду
 
-const newLvlDungeon = "\n";
 
-console.log('[lstLvl]', lastLvlDungeons );
 
-// * добавление каждому этажу # * i + абзац ( \n )
-for(let i = 1 ; i < lastLvlDungeons + 1 ; i++){
+
+// const lvl = "#";
+
+// const lastLvlDungeons = "######" .length;
+
+// let lvlDungeon = "";
+
+// const newLvlDungeon = "\n";
+
+// console.log('[lstLvl]', lastLvlDungeons );
+
+// // * добавление каждому этажу # * i + абзац ( \n )
+// for(let i = 1 ; i < lastLvlDungeons + 1 ; i++){
+
+//   lvlDungeon += lvl.repeat(i) + newLvlDungeon;
+// }
+// console.log(lvlDungeon)
+
+
+
+
+
+
+
+
+
+
+//  ДЗ номер 2 сделать мини елочку с пробелами
+
+
+const rowSharp = 8;
+const rowLength = 8;
+
+const space = " ";
+const symbol= "#";
+const breaks = "\n";
+
+let result = "";
+
+for (let i = 0; i < rowSharp; i++) {
+    let str = "";
+
+    if ( i % 2 === 0) {
+      str += space ;
+} else{
+      // str += symbol;
+  }
+
+    for( let j = 0 ; j < rowLength ; j++){
+
+    if ( j % 2 === 0) {
+          str += space ;
+    } else{
+          str += symbol;
+      }
+    }
+
+    result += str + breaks;
   
-  lvlDungeon += lvl.repeat(i) + newLvlDungeon;
 }
-console.log(lvlDungeon)
+console.log(result)
