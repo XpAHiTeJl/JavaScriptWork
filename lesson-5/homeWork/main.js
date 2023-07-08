5dba31136fb7e198cae736dd0789952b216663e9
XpAHiTeJl <robengud2@gmail.com>
2023-07-08 23:44:00 +0300

    home work-6

--- lesson-5/homeWork/main.js
+++ lesson-5/homeWork/main.js
@@ -1,24 +1,53 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+confirm ("Hello Everyone")
 
-setupCounter(document.querySelector('#counter'))
+let isRunning = true;
+
+ while(isRunning) {
+    const choice = interfaceSait();
+
+    switch (choice) {
+      case "a":
+          const userName = prompt("Whats is your name? ");
+          const cardNumber = prompt("Whats is your card number? ");
+          const userData = prompt("Whats is your data? ");
+          const userCVV = prompt("Whats is your CVV? ");
+
+          const person = createdPerson(userName,cardNumber,userData,userCVV);
+          console.log(person)
+        break;
+      case "b":
+         alert("no bay")
+        break;
+      case "q":
+        isRunning = false;
+        break;
+
+      default:
+        break;
+    }break;
+    
+ }
+
+
+ function validateCVV(CVV) {
+    if(userCVV.length = 3);  
+}
+
+function cardMaxNumber (){
+
+};
+
+ function createdPerson(userName,cardNumber,userData,userCVV) {
+      return {
+        "Имя": userName,
+        "Номер" : cardNumber,
+        "Дата" : userData,
+        "CVV" : userCVV
+      }
+    }
+
+function interfaceSait() {
+  return prompt ("a)Get Users b) New scum operation q) quit")
+};
