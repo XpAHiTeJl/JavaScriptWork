f7efc78222ed5f6a8580b0156610f6a5828c0910
XpAHiTeJl <robengud2@gmail.com>
2023-07-25 23:02:41 +0300

    nemohomeWork

--- lesson-7/homeWork/main.js
+++ lesson-7/homeWork/main.js
@@ -1,2 +1,58 @@
-СОбрание lesson 11 с немо
-+ lesson 13
+// СОбрание lesson 11 с немо
+// + lesson 13
+
+
+const searchNemo = "asdsadwqeqwnemodadwqetgsdhqe";
+console.log("Search nemo this text: ",searchNemo);
+
+const nameNemo = "nemo";
+
+const startIndex = searchNemo.indexOf(nameNemo);
+const lastIndex = startIndex + nameNemo.length;
+
+console.log('[startIndex]',startIndex);
+console.log('[lastIndex]',lastIndex);
+
+const Nemo = searchNemo.slice(startIndex, lastIndex );
+console.log('This is : '+ Nemo);
+
+
+console.log("----------------------------------------");
+const findNemo = [
+    'don',
+    'nemo',
+    'don',
+    'nemo',
+    'don',
+    'don',
+    'nemo',
+    'don',
+    'nemo',
+    'don'
+]
+console.log(findNemo);
+
+for (let i = 0; i < findNemo.length; i++) {
+    const thiesNemo = findNemo[i];
+    
+    if (thiesNemo === 'nemo') {
+
+        console.log("Yep this : " + thiesNemo);
+        console.log("Nemo in position : " + i );
+        
+    }
+}    
+console.log("----------------------------------------");    
+
+
+const nemo = {
+    firstName : {
+        surname : {
+            text : {
+                msg : "nameNemo"
+            }
+        } 
+    }
+}
+console.log(nemo.firstName.surname.text.msg)
+console.log("----------------------------------------");    
