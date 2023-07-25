1e9ff0035bad56f3da3fa2f9cb357b6b1b163c9b
XpAHiTeJl <robengud2@gmail.com>
2023-07-26 01:12:27 +0300

    hw

--- lesson-10/homeWork/main.js
+++ lesson-10/homeWork/main.js
@@ -1,1 +1,52 @@
 "use strict";
+console.log("----------------------------------------");
+
+function calc() {
+
+  const num1 = getNumber();
+  const num2 = getNumber();
+
+  const number = prompt("Какие символы ты хочешь ввести? ");
+  console.log(number);
+
+
+  switch (number) {
+    case "+":
+        alert(num1 + num2);
+      break;
+  
+    case "-":
+        alert(num1 - num2);
+      break;
+  
+    case "/":
+        alert(num1 / num2);
+      break;
+  
+    case "*":
+        alert(num1 * num2);
+      break;
+  
+    default:
+      alert("Введи корректное ЧИСЛО! ")
+      break;
+  }
+
+  function isInvalid(num) {
+    return isNaN(num)
+}
+
+  function getNumber() {
+    let trueFunk = true;
+
+    while (trueFunk) {
+      const num = +prompt("Введи число : ");
+      trueFunk = isInvalid(num);
+      if (!toggler) return num;
+            
+            if(toggler) {
+                alert("Ты должен ввести корректное ЧИСЛО! ");
+            }
+    }
+  }
+}
