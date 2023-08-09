6110c2cc67a9e88a22079c97d2e84c71bf15aa37
XpAHiTeJl <robengud2@gmail.com>
2023-08-10 00:36:56 +0300

    storeIfilms

--- lesson-11/homeWork/main.js
+++ lesson-11/homeWork/main.js
@@ -1,24 +1,106 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+"use strict";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const store = [
+  {
+  category : "drama",
+  movie : [ 
+      {
+        label : "Barbie",
+        yearOfRelease : new Date().getFullYear().toString(),
+        tags : ["#iCan'tBelieveIt" , "#it'sAmazing"]
+      },
+      {
+        label : "Sparta",
+        yearOfRelease : new Date().getFullYear().toString(),
+        tags : ["#rome" , "#gladiator" , "#misha"]
+          }
+        ]
+      },
 
-setupCounter(document.querySelector('#counter'))
+      {
+      category : "comedy",
+      movie : [ 
+      {
+        label : "FunnyShit",
+        yearOfRelease : new Date().getFullYear().toString(),
+        tags : [ "#it'cool"]
+      },
+      {
+        label : "FunnyShit2",
+        yearOfRelease : new Date().getFullYear().toString(),
+        tags : ["#dnepr" , "#office"]
+          }
+        ]
+      }
+];
+
+  let isRunning = true;
+
+  const isArray = Array.isArray;
+
+ while (isRunning) {
+    const movie = prompt(" Привет, ты должен выбрать категорию: " + getCategories(store));
+    console.log('[категория]', movie);
+
+    matchCategories(movie,store);
+  
+    isRunning = isExit();
+    
+  } 
+ 
+  function getCategories(arr) {
+//! Проверка на масив
+     if (!isArray(arr)) {
+          return;
+     };
+// !-------------------
+     const categories = [];
+     
+     for (const element of arr) {
+       if(!'category' in element) {
+        continue;
+       };
+       categories.push(element.category);
+
+      // console.log('[element]', element);
+      
+     }
+      return categories.join(", ");
+     
+  }
+
+  function matchCategories(pickedCategory, arr) {
+    if (!isArray(arr)) {
+      return;
+    };
+
+     for (let i = 0; i < arr.length; i++) {
+      const category = arr[i];
+
+      const hasCategory = category.category === pickedCategory;
+
+              if(!hasCategory) continue;
+      
+              console.log("Фильмы в выбранной категории - ",category.movie);
+     }
+    
+
+    
+
+  
+  }
+
+  function isExit() {
+     return !confirm ( " Ты  хочешь уйти  ? ")
+    
+  }
+
+ for (let i = 0; i < store.length; i++) {
+    const movie = store[i];
+    console.log('[категории]', movie);
+  }
+
+ const result = getCategories(store); 
+ console.log('[result]',result);
+
+        console.log('[store]', store);
