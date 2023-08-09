c4ae8bc12950fa2e2d96c15a9c6a9cf5a72b0383
XpAHiTeJl <robengud2@gmail.com>
2023-08-09 21:53:32 +0300

    calkulator

--- lesson-10/homeWork/main.js
+++ lesson-10/homeWork/main.js
@@ -3,50 +3,81 @@ console.log("----------------------------------------");
 
 function calc() {
 
-  const num1 = getNumber();
-  const num2 = getNumber();
+  const num1 = getNumber1();
+  console.log("Первое число :",num1);
+  const num2 = getNumber2();
+  console.log("Второе число :",num2);
 
-  const number = prompt("Какие символы ты хочешь ввести? ");
-  console.log(number);
+
+  const number = prompt("Какие символы ты хочешь ввести? ( + ) ( - ) ( / ) ( * ) ");
+  console.log("Твой Символ : ",number);
 
 
   switch (number) {
     case "+":
+      console.log("Ответ: ",num1 + num2 );
         alert(num1 + num2);
+
       break;
   
     case "-":
+      console.log("Ответ: ",num1 - num2 );
         alert(num1 - num2);
+      
+
       break;
   
     case "/":
+      console.log("Ответ: ",num1 / num2 );
         alert(num1 / num2);
+      
+
       break;
   
     case "*":
+      console.log("Ответ: ",num1 * num2 );
         alert(num1 * num2);
+      
+
       break;
   
     default:
       alert("Введи корректное ЧИСЛО! ")
       break;
+
   }
+  
 
   function isInvalid(num) {
     return isNaN(num)
 }
 
-  function getNumber() {
+  function getNumber1() {
     let trueFunk = true;
 
     while (trueFunk) {
-      const num = +prompt("Введи число : ");
+      const num = +prompt("Введи первое число : ");
       trueFunk = isInvalid(num);
-      if (!toggler) return num;
+      if (!trueFunk) return num;
             
-            if(toggler) {
+            if(trueFunk) {
+                alert("Ты должен ввести корректное ЧИСЛО! ");
+            }
+    }
+  }
+  function getNumber2() {
+    let trueFunk = true;
+
+    while (trueFunk) {
+      const num = +prompt("Введи второе число : ");
+      trueFunk = isInvalid(num);
+      if (!trueFunk) return num;
+            
+            if(trueFunk) {
                 alert("Ты должен ввести корректное ЧИСЛО! ");
             }
     }
   }
 }
+calc();
+console.log("----------------------------------------");
