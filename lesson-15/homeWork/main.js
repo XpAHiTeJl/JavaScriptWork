6f6230075091c4e6bc06ce5085801718240ba844
XpAHiTeJl <robengud2@gmail.com>
2023-09-07 01:36:04 +0300

    homeWorkTextTest

---
+++ lesson-15/homeWork/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// Task
+
+// Create following DOM structure via JS :
+
+//             <head>|
+// <html> ---->
+//             <body>|
+//                         <div id="chat"> ->
+//                                             screen - > div. class = "screen" ,
+//                                             backgroud = darkmagenta ;
+
+//                                         keyboard with emojies : lol , kek  ... etc
+
+//                                         gif : some gifs
+
+//                         <div class="main-keyboard">
+//                                                     -> every letter on a keyboard
+//             </body>|
+// 1) Is main-keyboard parent of letter ?
+// main-keyboard Родительский компонент по соотношению к latter.
+
+// 2) Is chat child of body?
+// По сколько chat уже находится в родителе body по древообразной структуре он в него входит.
+
+// 3) What is difference between "HTMLCollection" and "NodeList"?
+// HTMLCollection; Что то живое в коде, что может меняться, а NodeList это подобие скриншота с прогрузки HTML с которым можно работать.
+
+// 4) What does class in JS do?
+// используются для работы объектов с помощью конструктора, определения их полей и методов.
+
+// 5) Why sould we use classes ?
+//  способ упростить код при использовании прототипов и наследования и сделать его более симпатичным.
+
+// 6) What is DOM in JS ? Do you think it's object ?
+// Это структура HTML  только используемая в Java Skript, все что подтягивается из JS является всегда объектом
+
+// 7) How many types of function do you know ? decl , exp , an
+//Замыкающая функция, Возвращающая функция , стрелочная функция, Объявление функций, Выражающая функция, Анонимная функция.
+
+// 8) What are diffecrences between class and constructor and simple function ?
+// Классы дают возможность использовать насследия, так же преимущества классов в том, что в функции нету конструкторов по умолчанию.
+
+// 9) DO you think classes useful ?
+//  классом удобно пользоваться в обычном ЖС,
