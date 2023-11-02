d098fcf592d8f2789e6db0f7f087dbf8f8165d55
XpAHiTeJl <robengud2@gmail.com>
2023-11-02 20:46:07 +0200

    button

--- lesson-23/homeWork/component/index.js
+++ lesson-23/homeWork/component/index.js
@@ -2,5 +2,7 @@ import { App } from "./App";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Main } from "./Main";
+import { Button } from "./Button";
+import { Input } from "./input";
 
-export { App, Header, Footer, Main };
+export { App, Header, Footer, Main, Button, Input };
