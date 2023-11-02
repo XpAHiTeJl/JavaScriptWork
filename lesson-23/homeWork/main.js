26293bd5cbd76ca02951cfc66d9f5d40da3b851b
XpAHiTeJl <robengud2@gmail.com>
2023-11-02 19:04:34 +0200

    homeVlog

--- lesson-23/homeWork/main.js
+++ lesson-23/homeWork/main.js
@@ -20,7 +20,50 @@ const root = document.querySelector("#app");
 
 const app = new Application({});
 
-app.addChildren([new Header({}), new Main({}), new Footer({})]);
+app.addChildren([
+  new Header({
+    children: [
+      new Component({
+        tagName: "a",
+        className: "header-link",
+        textContent: "About us ",
+        events: {
+          click: (e) => {
+            render(
+              main,
+              new Component({
+                tagName: "div",
+                html: `<h2>About US </h2>`,
+              }).toHTML()
+            );
+          },
+        },
+      }),
+      toHTML(),
+      new Component({
+        tagName: "a",
+        className: "header-link",
+        textContent: "Login",
+        events: {
+          click: (e) => {
+            render(
+              main,
+              new Component({
+                tagName: "div",
+                html: `<h2>Login</h2>`,
+              }).toHTML()
+            );
+          },
+        },
+      }),
+      toHTML(),
+    ],
+  }),
+  new Main({
+    children: [],
+  }),
+  new Footer({}),
+]);
 
 const App = app.toHTML();
 
@@ -39,13 +82,13 @@ appendElement(
     <h3 class='person-data'>Age : ${player.getAge()}</h3>
     <h3 class='person-data'>Salary : ${player.getSalary()}</h3>
     <h3 class='person-data'>Balance : ${player.getBalance()}</h3>
-   
+
     <h3 class='person-data'>Relations : ${
       player.getRel() ? player.getRel() : "Single"
     }</h3>
 
     <div class="person-img-wrapper">
-        <img class='person-img' src="${player.getAvatar()}"/> 
+        <img class='person-img' src="${player.getAvatar()}"/>
     </div>
     `,
   }).toHTML()
