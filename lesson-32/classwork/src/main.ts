7ece25db987b7454bb53d13c9426a0a9606da0c8
XpAHiTeJl <robengud2@gmail.com>
2023-11-16 13:37:21 +0200

    value

--- lesson-32/classwork/src/main.ts
+++ lesson-32/classwork/src/main.ts
@@ -7,18 +7,10 @@ const name = document.querySelector("#Name");
 const age = document.querySelector("#Age");
 const select = document.querySelector("#select");
 const checkbox = document.querySelector("#scales");
+const checkboxredy = document.querySelector(".checkbox-redy");
 const namemain = document.querySelector(".main-name");
-
-addperson.addEventListener("click", (e) => {
-  console.log("Имя:", name.value);
-  console.log("Возраст:", age.value);
-  console.log("Пол:", select.value);
-  console.log("Готовность:", checkbox.checked);
-
-  namemain?.appendChild(
-    document.createTextNode(document.querySelector("#Name").value)
-  );
-});
+const mainage = document.querySelector(".main-age");
+const maingender = document.querySelector(".main-gender");
 
 addbtn.addEventListener("click", (e) => {
   if (formPerson.className.includes("--active")) {
@@ -27,3 +19,45 @@ addbtn.addEventListener("click", (e) => {
     formPerson.className = "formPersonl--active";
   }
 });
+
+addperson.addEventListener("click", (e) => {
+  console.log("Имя:", name.value);
+  console.log("Возраст:", age.value);
+  console.log("Пол:", select.value);
+  console.log("Готовность:", checkbox.checked);
+
+  // namemain.appendChild(
+  //   document.createTextNode(document.querySelector("#Name").value)
+  // );
+  // mainage.appendChild(
+  //   document.createTextNode(document.querySelector("#Age").value)
+  // );
+  // maingender.appendChild(
+  //   document.createTextNode(document.querySelector("#select").value)
+  // );
+  // checkboxredy.appendChild(
+  //   document.createTextNode(document.querySelector("#scales").checked)
+  // );
+
+  appendElement(namemain, document.querySelector("#Name").value);
+  appendElement(mainage, document.querySelector("#Age").value);
+  appendElement(maingender, document.querySelector("#select").value);
+  appendElement(checkboxredy, document.querySelector("#scales").checked);
+});
+
+// function addRow(tableID) {
+//   const tableRef = document.getElementById(tableID);
+
+//   var newRow = tableRef.insertRow(1);
+
+//   var newCell = newRow.insertCell(0);
+
+//   var newText = document.createTextNode("New  row");
+
+//   newCell.appendChild(newText);
+// }
+// addRow("tablea");
+
+const appendElement = (node, element) => {
+  node.append(element);
+};
