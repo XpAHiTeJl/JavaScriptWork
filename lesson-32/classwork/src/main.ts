fc78f3e6b68451e97abeb4cb0b7be78a90a4fbee
XpAHiTeJl <robengud2@gmail.com>
2023-11-17 01:34:08 +0200

    added

--- lesson-32/classwork/src/main.ts
+++ lesson-32/classwork/src/main.ts
@@ -11,6 +11,7 @@ const checkboxredy = document.querySelector(".checkbox-redy");
 const namemain = document.querySelector(".main-name");
 const mainage = document.querySelector(".main-age");
 const maingender = document.querySelector(".main-gender");
+const tablea = document.querySelector("#tablea");
 
 addbtn.addEventListener("click", (e) => {
   if (formPerson.className.includes("--active")) {
@@ -25,38 +26,76 @@ addperson.addEventListener("click", (e) => {
   console.log("Возраст:", age.value);
   console.log("Пол:", select.value);
   console.log("Готовность:", checkbox.checked);
+  console.log("--------------------");
+  // nameprofil();
+  // ageprofil();
+  // genderprofil();
+  // redyprofile();
+  const block = document.createElement("div");
+  block.className = "main-profile";
+  appendElement(tablea, block);
+  nameprofil();
+  ageprofil();
+  genderprofil();
+  redyprofile();
+  // appendElement(namemain, document.querySelector("#Name").value);
+  document.querySelector("#Name").value = "";
 
-  // namemain.appendChild(
-  //   document.createTextNode(document.querySelector("#Name").value)
-  // );
-  // mainage.appendChild(
-  //   document.createTextNode(document.querySelector("#Age").value)
-  // );
-  // maingender.appendChild(
-  //   document.createTextNode(document.querySelector("#select").value)
-  // );
-  // checkboxredy.appendChild(
-  //   document.createTextNode(document.querySelector("#scales").checked)
-  // );
+  // appendElement(mainage, document.querySelector("#Age").value);
+  document.querySelector("#Age").value = "";
 
-  appendElement(namemain, document.querySelector("#Name").value);
-  appendElement(mainage, document.querySelector("#Age").value);
-  appendElement(maingender, document.querySelector("#select").value);
-  appendElement(checkboxredy, document.querySelector("#scales").checked);
+  // appendElement(maingender, document.querySelector("#select").value);
+  document.querySelector("#select").value = "";
+
+  // appendElement(checkboxredy, document.querySelector("#scales").checked);
+  document.querySelector("#scales").checked = "";
 });
 
-// function addRow(tableID) {
-//   const tableRef = document.getElementById(tableID);
+function nameprofil() {
+  let value = document.getElementById("Name").value;
+  if (!value) return;
+  let container = document.createElement("td");
+  container.className = "td";
+  container.innerHTML = value;
+  document.querySelector(".main-profile").appendChild(container);
+}
+function ageprofil() {
+  let value = document.getElementById("Age").value;
+  if (!value) return;
+  let container = document.createElement("td");
+  container.className = "td";
+  container.innerHTML = value;
+  document.querySelector(".main-profile").appendChild(container);
+}
+function genderprofil() {
+  let value = document.getElementById("select").value;
+  if (!value) return;
+  let container = document.createElement("td");
+  container.className = "td";
+  container.innerHTML = value;
+  document.querySelector(".main-profile").appendChild(container);
+}
+function redyprofile() {
+  let value = document.getElementById("scales").checked;
+  if (!value) return;
+  let container = document.createElement("td");
+  container.className = "td";
+  container.innerHTML = value;
+  document.querySelector(".main-profile").appendChild(container);
+}
 
-//   var newRow = tableRef.insertRow(1);
-
-//   var newCell = newRow.insertCell(0);
-
-//   var newText = document.createTextNode("New  row");
-
-//   newCell.appendChild(newText);
-// }
-// addRow("tablea");
+// namemain.appendChild(
+//   document.createTextNode(document.querySelector("#Name").value)
+// );
+// mainage.appendChild(
+//   document.createTextNode(document.querySelector("#Age").value)
+// );
+// maingender.appendChild(
+//   document.createTextNode(document.querySelector("#select").value)
+// );
+// checkboxredy.appendChild(
+//   document.createTextNode(document.querySelector("#scales").checked)
+// );
 
 const appendElement = (node, element) => {
   node.append(element);
