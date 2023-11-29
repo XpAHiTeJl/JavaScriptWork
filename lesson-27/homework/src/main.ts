645132c42420ea29699f1297457f9ccf62dd8247
XpAHiTeJl <robengud2@gmail.com>
2023-11-29 20:49:55 +0200

    set

--- lesson-27/homework/src/main.ts
+++ lesson-27/homework/src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+
+import { App } from "../apps";
+import { setSpinner } from "../store";
+import { setInfoBox } from "./store/getInfoBox";
+import "./style.css";
+
+setSpinner();
+setInfoBox();
+
+const appComp = new App();
+appComp.load();
