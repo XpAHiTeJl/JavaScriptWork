99e3484f53188d739f878d2e40ea6fd97bc56cf3
XpAHiTeJl <robengud2@gmail.com>
2023-12-05 22:38:58 +0200

    newSahmat

--- lesson-36/classwork/src/main.ts
+++ lesson-36/classwork/src/main.ts
@@ -1,10 +1,56 @@
 import "./style.css";
-const doskasahmat = document.getElementById("board");
+const chessboardElement = document.getElementById("chessboard");
+const sahmatElement = document.getElementById("sahmat");
+const bukviPoleElement = document.getElementById("polebukvi");
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+let selectedPiece = null;
 
-const bukvi = ["A", "B", "C", "D", "E", "F", "G", "H"];
+for (let row = 0; row < 8; row++) {
+  for (let col = 0; col < 8; col++) {
+    const cell = document.createElement("div");
+    const isWhite = (row + col) % 2 === 0;
+    cell.className = isWhite ? "white" : "black";
 
-for (let stroka = 0; stroka < 8; stroka++) {
-  const bukvishahmat = document.createElement("div");
-  bukvishahmat.innerHTML = `<span>${bukvi[stroka]}</span>`;
-  doskasahmat.append(bukvishahmat);
+    cell.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    cell.addEventListener("drop", (e) => {
+      e.preventDefault();
+      if (selectedPiece) {
+        const targetCell = e.target;
+        targetCell.appendChild(selectedPiece);
+      }
+    });
+
+    chessboardElement.appendChild(cell);
+  }
+}
+
+for (let col = 0; col < 8; col++) {
+  const letterCell = document.createElement("div");
+  letterCell.className = col % 2 === 0;
+  letterCell.innerHTML = `<span>${letters[col]}</span>`;
+  bukviPoleElement.appendChild(letterCell);
+}
+
+// Place a king piece on the board
+const king = createPiece("white", "♔");
+sahmatElement.appendChild(king);
+
+function createPiece(color, type) {
+  const piece = document.createElement("div");
+  piece.className = `piece ${color}`;
+  piece.textContent = type;
+  piece.draggable = true;
+
+  piece.addEventListener("dragstart", (e) => {
+    selectedPiece = e.target;
+  });
+
+  piece.addEventListener("dragend", () => {
+    selectedPiece = null;
+  });
+
+  return piece;
 }
