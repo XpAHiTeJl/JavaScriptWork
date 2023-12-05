f79dfb3d7f66c8e092199d42e0d0cc039753d5d0
XpAHiTeJl <robengud2@gmail.com>
2023-12-05 23:41:56 +0200

    finish

--- lesson-36/classwork/src/main.ts
+++ lesson-36/classwork/src/main.ts
@@ -1,12 +1,12 @@
 import "./style.css";
-const chessboardElement = document.getElementById("chessboard");
+const boardElement = document.getElementById("board");
 const sahmatDownElement = document.getElementById("sahmatdown");
 const sahmatupElement = document.getElementById("sahmatup");
 const bukviPoleElement = document.getElementById("polebukvi");
 const numberssboku = document.querySelector(".numberssboku");
-const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 let selectedPiece = null;
 
+// Доска
 for (let row = 0; row < 8; row++) {
   for (let col = 0; col < 8; col++) {
     const cell = document.createElement("div");
@@ -25,27 +25,44 @@ for (let row = 0; row < 8; row++) {
       }
     });
 
-    chessboardElement.appendChild(cell);
+    boardElement.appendChild(cell);
   }
 }
-
+// Буквы
 for (let col = 0; col < 8; col++) {
+  const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
   const letterCell = document.createElement("div");
   letterCell.className = col % 2 === 0;
   letterCell.innerHTML = `<span>${letters[col]}</span>`;
   bukviPoleElement.appendChild(letterCell);
 }
-
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
+// Цифры
 for (let col = 0; col < 8; col++) {
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
   const letterCell = document.createElement("div");
   letterCell.className = col % 2 === 0;
   letterCell.innerHTML = `<span>${numbers[col]}</span>`;
   numberssboku.appendChild(letterCell);
 }
+// Функция для создания фигур
+function createPiece(color, type) {
+  const piece = document.createElement("div");
+  piece.className = `piece ${color}`;
+  piece.textContent = type;
+  piece.draggable = true;
+
+  piece.addEventListener("dragstart", (e) => {
+    selectedPiece = e.target;
+  });
+
+  piece.addEventListener("dragend", () => {
+    selectedPiece = null;
+  });
+
+  return piece;
+}
 
 // тут добавлять в верхний отсек шахматы
-
 const kingUp = createPiece("black", "♔");
 sahmatupElement.appendChild(kingUp);
 const queenUp = createPiece("black", "♛");
@@ -85,20 +102,32 @@ const kingDown = createPiece("white", "♔");
 sahmatDownElement.appendChild(kingDown);
 const queenDown = createPiece("white", "♛");
 sahmatDownElement.appendChild(queenDown);
-
-function createPiece(color, type) {
-  const piece = document.createElement("div");
-  piece.className = `piece ${color}`;
-  piece.textContent = type;
-  piece.draggable = true;
-
-  piece.addEventListener("dragstart", (e) => {
-    selectedPiece = e.target;
-  });
-
-  piece.addEventListener("dragend", () => {
-    selectedPiece = null;
-  });
-
-  return piece;
-}
+const ladiyaDown1 = createPiece("white", "♜");
+sahmatDownElement.appendChild(ladiyaDown1);
+const ladiyaDown2 = createPiece("white", "♜");
+sahmatDownElement.appendChild(ladiyaDown2);
+const konDown1 = createPiece("white", "♞");
+sahmatDownElement.appendChild(konDown1);
+const konDown2 = createPiece("white", "♞");
+sahmatDownElement.appendChild(konDown2);
+const slonDown1 = createPiece("white", "♝");
+sahmatDownElement.appendChild(slonDown1);
+const slonDown2 = createPiece("white", "♝");
+sahmatDownElement.appendChild(slonDown2);
+//
+const peshkaDown1 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown1);
+const peshkaDown2 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown2);
+const peshkaDown3 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown3);
+const peshkaDown4 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown4);
+const peshkaDown5 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown5);
+const peshkaDown6 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown6);
+const peshkaDown7 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown7);
+const peshkaDown8 = createPiece("white", "♟");
+sahmatDownElement.appendChild(peshkaDown8);
