501685d1818e795d4c254c5f35c4ffbcc818341f
XpAHiTeJl <robengud2@gmail.com>
2023-12-05 23:26:12 +0200

    ups

--- lesson-36/classwork/src/main.ts
+++ lesson-36/classwork/src/main.ts
@@ -44,18 +44,47 @@ for (let col = 0; col < 8; col++) {
   numberssboku.appendChild(letterCell);
 }
 
-// тут добавлять в нижний отсек шахматы
-const king = createPiece("white", "♔");
-sahmatDownElement.appendChild(king);
-const queen = createPiece("white", "♛");
-sahmatDownElement.appendChild(queen);
-
 // тут добавлять в верхний отсек шахматы
 
-const king1 = createPiece("black", "♔");
-sahmatupElement.appendChild(king1);
-const queen1 = createPiece("black", "♛");
-sahmatupElement.appendChild(queen1);
+const kingUp = createPiece("black", "♔");
+sahmatupElement.appendChild(kingUp);
+const queenUp = createPiece("black", "♛");
+sahmatupElement.appendChild(queenUp);
+const ladiyaUp1 = createPiece("black", "♜");
+sahmatupElement.appendChild(ladiyaUp1);
+const ladiyaUp2 = createPiece("black", "♜");
+sahmatupElement.appendChild(ladiyaUp2);
+const konUp1 = createPiece("black", "♞");
+sahmatupElement.appendChild(konUp1);
+const konUp2 = createPiece("black", "♞");
+sahmatupElement.appendChild(konUp2);
+const slonUp1 = createPiece("black", "♝");
+sahmatupElement.appendChild(slonUp1);
+const slonUp2 = createPiece("black", "♝");
+sahmatupElement.appendChild(slonUp2);
+
+const peshkaUp1 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp1);
+const peshkaUp2 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp2);
+const peshkaUp3 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp3);
+const peshkaUp4 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp4);
+const peshkaUp5 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp5);
+const peshkaUp6 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp6);
+const peshkaUp7 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp7);
+const peshkaUp8 = createPiece("black", "♟");
+sahmatupElement.appendChild(peshkaUp8);
+
+// тут добавлять в нижний отсек шахматы
+const kingDown = createPiece("white", "♔");
+sahmatDownElement.appendChild(kingDown);
+const queenDown = createPiece("white", "♛");
+sahmatDownElement.appendChild(queenDown);
 
 function createPiece(color, type) {
   const piece = document.createElement("div");
