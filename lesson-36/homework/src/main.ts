5b1d13536831bbd7fb9d6a9b265991455f3c3ac8
XpAHiTeJl <robengud2@gmail.com>
2023-12-12 15:28:56 +0200

    interf

--- lesson-36/homework/src/main.ts
+++ lesson-36/homework/src/main.ts
@@ -1,1 +1,11 @@
 import "./style.css";
+import { Component, append, render } from "../core/index";
+
+const app = document.getElementById("app");
+
+const modal = new Component({
+  className: "block",
+  textContent: "qwe",
+});
+
+append(app, modal);
