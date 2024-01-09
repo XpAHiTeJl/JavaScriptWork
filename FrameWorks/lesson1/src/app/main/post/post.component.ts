e77eac08f632a3715af4e851d44729fb629d1bea
XpAHiTeJl <robengud2@gmail.com>
2024-01-09 20:22:23 +0200

    post main

--- FrameWorks/lesson1/src/app/main/post/post.component.ts
+++ FrameWorks/lesson1/src/app/main/post/post.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  toggler: boolean = false;
 
-  constructor() { }
+  todoList: string[] = [];
+  newTodo: string = '';
 
-  ngOnInit(): void {
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todoList.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
   }
 
+  removeTodo(index: number) {
+    this.todoList.splice(index, 1);
+  }
+
+  ngOnInit(): void {}
 }
