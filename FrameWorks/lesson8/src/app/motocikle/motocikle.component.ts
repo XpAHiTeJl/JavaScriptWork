cd717852b8a7521a9cf0787f9c92e2e321b707f3
XpAHiTeJl <robengud2@gmail.com>
2024-02-19 00:21:48 +0200

    finish

--- FrameWorks/lesson8/src/app/motocikle/motocikle.component.ts
+++ FrameWorks/lesson8/src/app/motocikle/motocikle.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Observable, interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-motocikle',
@@ -6,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './motocikle.component.scss',
 })
 export class MotocikleComponent implements OnInit {
+  toggler: boolean = true;
+  buyitem: boolean = false;
+
+  timer$: Observable<number>;
+
   slides: any[] = new Array(3).fill({
     id: -1,
     src: '',
     title: '',
     subtitle: '',
   });
+
   ngOnInit(): void {
     this.slides[0] = {
       src: './assets/img/angular.jpg',
@@ -22,5 +30,15 @@ export class MotocikleComponent implements OnInit {
     this.slides[2] = {
       src: './assets/img/vue.jpg',
     };
+    this.timer$ = interval(1000).pipe(
+      take(60),
+      map((x) => 60 - x)
+    );
+
+    this.timer$.subscribe((time) => {
+      if (time <= 0) {
+        this.toggler = false;
+      }
+    });
   }
 }
