39c7628b62841fc036441d2416661bed57d734b8
XpAHiTeJl <robengud2@gmail.com>
2024-02-28 23:23:04 +0200

    fix

--- FrameWorks/React/lesson1/src/App.jsx
+++ FrameWorks/React/lesson1/src/App.jsx
@@ -1,20 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <header>
-      <h1>Заголовок</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="">121321</a>
-          </li>
-          <li></li>
-          <li></li>
-        </ul>
-      </nav>
-    </header>
-  );
+  return <button></button>;
 }
 
 export default App;
---
+++ FrameWorks/React/lesson1/src/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Заголовок</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="">121321</a>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
