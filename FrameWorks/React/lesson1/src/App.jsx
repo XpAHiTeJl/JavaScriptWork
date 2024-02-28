53250da851793d47e28f2d2da95b78b938a06488
XpAHiTeJl <robengud2@gmail.com>
2024-02-28 23:11:58 +0200

    react

---
+++ FrameWorks/React/lesson1/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <header>
+      <h1>Заголовок</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="">121321</a>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
